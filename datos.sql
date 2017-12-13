f48f9d0ea8e44a01e258334f6e1da48d53e56d21
Jazmin Moreno <amoreno2@ucol.mx>
2017-12-13 05:10:15 +0000

    Issues 2

--- datos.sql
+++ datos.sql
@@ -2,6 +2,6 @@ CREATE TABLE `datos` (
   `id` int(8) unsigned NOT NULL auto_increment,
   `nombre` varchar(70) default NULL,
   `direccion` varchar(120) default NULL,
-  `fechaCreacion` timestamp NOT NULL,
+  `fechaCreacionn` timestamp NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
